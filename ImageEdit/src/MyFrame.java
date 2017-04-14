31cd715d396cf39d66f5eda7b603508279f9ae21
KatarinaAet <Katarina.Kulagina97@yandex.ru>
2017-04-15 00:17:11 +0300

    Самое начало - попытка реализации, "Загрузить", "Сохранить", "Сохранить как..."

---
+++ ImageEdit/src/MyFrame.java
@@ -0,0 +1,16 @@
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame
+    {
+        public void paint(Graphics g)
+        {
+            super.paint(g);
+        }
+        public MyFrame(String title)
+        {
+            super(title);
+        }
+    }
+
